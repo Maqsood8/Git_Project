9e918d2e842791e69610c16438f7aeea6fb000ec
Mohammed Maqsood <106028418+Maqsood8@users.noreply.github.com>
2022-09-01 15:56:56 +0200

    Update add.py

--- scripts/add.py
+++ scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: surya")
-    sport = input("Please add your favourite sports name: cricket")
+    name = input("Please add your name: ")
+    sport = input("Please add your favourite sports name: ")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
